64c3ba4308c9750aa0c8a213c5328a9c16298170
Barun Debnath <barundebnath91@gmail.com>
2025-06-28 15:10:58 +0530

    init guardrail node after start node

--- server/graph/nodes/__init__.py
+++ server/graph/nodes/__init__.py
@@ -6,13 +6,15 @@ LangGraph workflow for categorizing and processing user requests.
 """
 
 from .start import start_node
+from .guardrail import guardrail_node
 from .categorization import categorization_node
 from .result import result_node
 from .error_handler import error_handler_node
 
 __all__ = [
     "start_node",
-    "categorization_node", 
+    "guardrail_node",
+    "categorization_node",
     "result_node",
     "error_handler_node"
 ]
